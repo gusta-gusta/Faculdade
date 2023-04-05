b21e7a25485b2cde0ad4e58cdddf6b5adcec49dd
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2023-04-05 17:02:14 -0300

    att

---
+++ 2_Semestre/PhpBanco/Modulo 3/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+$host = "localhost";
+$port = 3030;
+$user = "root";
+$pass = "";
+$name = "clientes";
+$dsn = "mysql:host=$host;port=$port;dbname=$name";  // String de conexão MySQL
+// $dsn = "pgsql:host=$host;port=$port;dbname=$name";  // String de conexão PGSQL
+
+?>
