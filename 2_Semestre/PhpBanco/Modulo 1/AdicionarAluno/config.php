cca047753c30817459f272ad7a8d1378f08e8e6b
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2023-04-19 20:17:30 -0300

    att

--- 2_Semestre/PhpBanco/Modulo 1/AdicionarAluno/config.php
+++ 2_Semestre/PhpBanco/Modulo 1/AdicionarAluno/config.php
@@ -1,9 +1,9 @@
 <?php 
 
 $host = "localhost";
-$port = 3030;
+$port = 3306;
 $user = "root";
-$pass = "";
+$pass = "(Ap@ch3)!1";
 $name = "escola";
 $dsn = "mysql:host=$host;port=$port;dbname=$name";  // String de conexão MySQL
 // $dsn = "pgsql:host=$host;port=$port;dbname=$name";  // String de conexão PGSQL
--- 2_Semestre/PhpBanco/Modulo 1/Exemplos/exemploMySQL.php
+++ 2_Semestre/PhpBanco/Modulo 1/Exemplos/exemploMySQL.php
@@ -2,23 +2,23 @@
 
 //Constantes para armazenamento das variáveis de conexão.
 define('HOST', '127.0.0.1');
-define('DBNAME', 'test');
-define('USER', 'user');
-define('PASSWORD', 'psswd');
+define('DBNAME', 'escola');
+define('USER', 'root');
+define('PASSWORD', '(Ap@ch3)!1)');
 
 //Conectando com o Servidor
 $conn = mysqli_connect(HOST, USER, PASSWORD, DBNAME) or die( ' Não foi possível conectar.' );
 
 //Realizando uma consulta no BD
-$instrucaoSQL = "Select nome, cpf, telefone From Cliente";
+$instrucaoSQL = "Select matricula, nome, entrada From Alunos";
 $stmt = mysqli_prepare($conn, $instrucaoSQL);
-mysqli_stmt_bind_result($stmt, $nome, $cpf, $tel);
+mysqli_stmt_bind_result($stmt, $matricula, $nome, $entrada);
 mysqli_stmt_execute($stmt);
 
 while (mysqli_stmt_fetch($stmt)) {
+	echo $matricula . "\t";
 	echo $nome . "\t";
-	echo $cpf . "\t";
-	echo $tel . "\n";
+	echo $entrada . "\n";
 }
 
 //Encerrando a conexão
